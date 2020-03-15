386271d8a2a94999ccca0aedb957f5f5a41dfe4e
Sergey Zh <zhemers@gmail.com>
2020-03-16 01:52:25 +0300

    Code refactoring

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # aws_get_metric_statistics
 
-aws_get_metric_statistics allows to collect AWS Cloudwatch data for the metrics VolumeWriteBytes, VolumeReadBytes, VolumeWriteOps, VolumeReadOps
-of specified EBS volume and specified time span that can be more then 1440 data points with 1 hour granularity. Results are saved to CSV files.
+aws_get_metric_statistics gathers AWS CloudWatch metrics specified by -namespace, -name and -metric-name parametrs and time span that can be more then 1440 data points with 1 hour granularity (more than 60 days).
+Results are printed to console or may be saved to CSV file with specified or default file name.
 Dates must be specified in the format 'YYYY-MM-DD'.
 
 ```console
-$ go run aws_get_metric_statistics.go 
-Some of mandatory parameter is absent: ["vol" "dates" "datee"]
-aws_get_metric_statistics gathers disk metrics of AWS EBS volume. 'aws' command line tool must be setted up before. Dates must be specified in format YYYY-MM-DD.
-Usage: aws_get_metric_statistics -vol vol-id -dates YYYY-MM-DD -datee YYYY-MM-DD
-  -datee string
-    	End date for gathered data
-  -dates string
-    	Start date for gathered data 
-  -vol string
-    	VolumeId of AWS EC2's EBS disk
+$ ./aws_get_metric_statistics
+aws_get_metric_statistics gathers AWS CloudWatch metrics specified by -namespace, -name and -metric-name parametrs for specified time span (see example below).
+'aws' command line tool must be setted up before. Dates must be specified in format YYYY-MM-DD.
+Version 0.0.1
+Usage: aws_get_metric_statistics -name <object type>,Value=<value> -start-time YYYY-MM-DD -end-time YYYY-MM-DD -metric-name <metric> -namespace <name>
+  -end-time string
+    	End date for gathered data period
+  -metric-name string
+    	Metric's name
+  -name string
+    	Object type/value to gather data, for example 'InstanceId,Value=i-012345' for EC2 instance
+  -namespace string
+    	Metric's namespace (default "EC2")
+  -out-file string
+    	Write data to file, value 'default' generates default file name
+  -start-time string
+    	Start date for gathered data period
 Example
-  aws_get_metric_statistics -vol vol-01234567890123456 -dates 2020-01-01 -datee 2020-03-04
-exit status 1
+  aws_get_metric_statistics -name InstanceId,Value=i-012345 -start-time 2019-11-29 -end-time 2020-03-04 -metric-name NetworkOut
 ```
--- aws_get_metric_statistics.go
+++ aws_get_metric_statistics.go
@@ -7,40 +7,45 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strconv"
+	// "strconv"
+	"sort"
 	"strings"
 	"time"
 )
 
-type aaStrToInt map[string][]int64
-
+const sVersion = "0.0.1"
 const iDataPointLimit = 1440 // number of datapoint (or hours) with 3600 period that equals 60 days
 const sTimeRfc3339 = "T00:00:00Z"
 
-var aParams = []string{"vol", "dates", "datee"}
+var aParams = []string{"name", "start-time", "end-time", "metric-name", "default"}
 var (
-	sVol    = flag.String(aParams[0], "", "VolumeId of AWS EC2's EBS disk")
-	sDateSt = flag.String(aParams[1], "", "Start date for gathered data ")
-	sDateEn = flag.String(aParams[2], "", "End date for gathered data")
+	sName      = flag.String(aParams[0], "", "Object type/value to gather data, for example 'InstanceId,Value=i-012345' for EC2 instance")
+	sDateSt    = flag.String(aParams[1], "", "Start date for gathered data period")
+	sDateEn    = flag.String(aParams[2], "", "End date for gathered data period")
+	sMetric    = flag.String(aParams[3], "", "Metric's name")
+	sOutFile   = flag.String("out-file", "", "Write data to file, value 'default' generates default file name")
+	sNamespace = flag.String("namespace", "EC2", "Metric's namespace")
 )
 var sHelp = `Example
-  %s -vol vol-01234567890123456 -dates 2020-01-01 -datee 2020-03-04`
+  %s -name InstanceId,Value=i-012345 -start-time 2019-11-29 -end-time 2020-03-04 -metric-name NetworkOut`
 
 var aStrTable = []string{
-	"%s gathers disk metrics of AWS EBS volume. 'aws' command line tool must be setted up before. Dates must be specified in format YYYY-MM-DD.\n",
+	`%s gathers AWS CloudWatch metrics specified by -namespace, -name and -metric-name parametrs (see example below).
+'aws' command line tool must be setted up before. Dates must be specified in format YYYY-MM-DD.
+Version %s` + "\n",
 	"Some of mandatory parameter is absent: %q\n"}
 
 func main() {
 	flag.Usage = func() {
 		path := strings.Split(os.Args[0], "/")
 		cmd := path[len(path)-1]
-		fmt.Fprintf(flag.CommandLine.Output(), aStrTable[0], cmd)
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s -vol vol-id -dates YYYY-MM-DD -datee YYYY-MM-DD\n", cmd)
+		fmt.Fprintf(flag.CommandLine.Output(), aStrTable[0], cmd, sVersion)
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s -name <object type>,Value=<value> -start-time YYYY-MM-DD -end-time YYYY-MM-DD -metric-name <metric> -namespace <name>\n", cmd)
 		flag.PrintDefaults()
 		fmt.Fprintf(flag.CommandLine.Output(), sHelp+"\n", cmd)
 	}
 	flag.Parse()
-	if *sVol == "" || *sDateSt == "" || *sDateEn == "" {
+	if *sName == "" || *sDateSt == "" || *sDateEn == "" || *sMetric == "" {
 		fmt.Printf(aStrTable[1], aParams)
 		flag.Usage()
 		os.Exit(1)
@@ -51,9 +56,7 @@ func main() {
 	tDiffHours := timeEn.Sub(timeSt).Hours()
 	fmt.Printf("ds=%q de=%q diff=%q\n", timeSt, timeEn, tDiffHours)
 
-	aVolRWBytes := make(aaStrToInt)
-	aVolRWOps := make(aaStrToInt)
-	// fmt.Printf("aVolRWBytes=%v\n", aVolRWBytes)
+	aaDateToVal := make(map[string][]string)
 
 	iCnt := int(tDiffHours) / iDataPointLimit
 	if int(tDiffHours)%iDataPointLimit > 0 {
@@ -65,63 +68,79 @@ func main() {
 	for i := 0; i < iCnt; i++ {
 		if t1.After(timeEn) {
 			t1 = timeEn
+			if t0 == t1 {
+				t0 = t0.AddDate(0, 0, -1)
+			}
 		}
-		// fmt.Printf("t0=%s t1=%s\n", t0.String()[:10], t1.String()[:10])
+		fmt.Printf("t0=%s t1=%s\n", t0.String()[:10], t1.String()[:10])
 
-		lines := aws_get_metric_statistics(*sVol, t0.String()[:10], t1.String()[:10], "VolumeWriteBytes")
-		out_to_array(lines, aVolRWBytes)
-		lines = aws_get_metric_statistics(*sVol, t0.String()[:10], t1.String()[:10], "VolumeReadBytes")
-		out_to_array(lines, aVolRWBytes)
-
-		lines = aws_get_metric_statistics(*sVol, t0.String()[:10], t1.String()[:10], "VolumeWriteOps")
-		out_to_array(lines, aVolRWOps)
-		lines = aws_get_metric_statistics(*sVol, t0.String()[:10], t1.String()[:10], "VolumeReadOps")
-		out_to_array(lines, aVolRWOps)
+		lines := aws_get_metric_statistics(*sName, t0.String()[:10], t1.String()[:10], *sMetric, *sNamespace)
+		// fmt.Printf("lines=%v\n", lines)
+		aws_out_to_array(lines, aaDateToVal)
 
 		t0 = t1.AddDate(0, 0, 1)
 		t1 = t0.AddDate(0, 0, 60)
 	}
 
-	sName := *sVol + "-VolumeBytes.csv"
-	f, _ := os.Create(sName)
-	defer f.Close()
-	f.WriteString(fmt.Sprintf("date,VolumeWriteBytes,VolumeReadBytes\n"))
-	for t := range aVolRWBytes {
-		t1 := t[:len(t)-1]
-		t1 = strings.ReplaceAll(t1, "T", " ")
-		f.WriteString(fmt.Sprintf("%s,%d,%d\n", t1, aVolRWBytes[t][0], aVolRWBytes[t][1]))
+	sFilename := ""
+	f := os.Stdout
+	if *sOutFile != "" {
+		sFilename = *sOutFile
+		if *sOutFile == aParams[4] {
+			sFilename = *sNamespace + "-" + *sMetric + "-" + *sName
+		}
+		sFilename = sFilename + ".csv"
+		f, _ = os.Create(sFilename)
+		defer f.Close()
 	}
-	fmt.Printf("VolumeWriteBytes/VolumeReadBytes data were saved to file %q\n", sName)
+	f.WriteString(fmt.Sprintf("date,%s\n", *sMetric))
+	// fmt.Printf("aaDateToVal=%v\n", aaDateToVal)
+	// Soerting keys(dates) of the aaDateToVal
+	keys := make([]string, 0, len(aaDateToVal))
+	for k := range aaDateToVal {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, keyTime := range keys {
+		t1 := keyTime[:len(keyTime)-1]
+		t1 = strings.ReplaceAll(t1, "T", " ")
+		// if len(aaDateToVal[t]) == 1 {
+		// 	aaDateToVal[t] = append(aaDateToVal[t], "-1")
+		// }
+		f.WriteString(fmt.Sprintf("%s,%s\n", t1, aaDateToVal[keyTime][0]))
+	}
+	if *sOutFile != "" {
+		fmt.Printf("Metric '%s/%s' data with name %q were saved to the file %q\n", *sNamespace, *sMetric, *sName, sFilename)
+	}
 
-	sName = *sVol + "-VolumeOps.csv"
-	f, _ = os.Create(sName)
-	f.WriteString(fmt.Sprintf("date,VolumeWriteOps,VolumeReadOps\n"))
-	defer f.Close()
-	for t := range aVolRWOps {
-		t1 := t[:len(t)-1]
-		t1 = strings.ReplaceAll(t1, "T", " ")
-		// fmt.Printf("%s,%f,%f\n", t1, aVolRWOps[t][0], aVolRWOps[t][1])
-		f.WriteString(fmt.Sprintf("%s,%d,%d\n", t1, aVolRWOps[t][0], aVolRWOps[t][1]))
-	}
-	fmt.Printf("VolumeWriteOps/VolumeReadOps data were saved to file %q\n", sName)
 }
 
-func out_to_array(out []string, arr aaStrToInt) {
+// func out_to_array(out []string, arr aaStrToInt) {
+// 	for l := range out {
+// 		line := out[l]
+// 		vals := strings.Split(line, "\t")
+// 		fVal, _ := strconv.ParseFloat(vals[1], 64)
+// 		arr[vals[2]] = append(arr[vals[2]], int64(fVal))
+// 	}
+// }
+
+func aws_out_to_array(out []string, arr map[string][]string) {
 	for l := range out {
 		line := out[l]
 		// fmt.Printf("%d %s\n", l, line)
 		vals := strings.Split(line, "\t")
 		// fmt.Printf("vals=%q\n", vals)
-		fVal, _ := strconv.ParseFloat(vals[1], 64)
-		arr[vals[2]] = append(arr[vals[2]], int64(fVal))
+		if vals[1] == "" {
+			vals[1] = "-1"
+		}
+		arr[vals[2]] = append(arr[vals[2]], vals[1])
 	}
-
 }
 
-func aws_get_metric_statistics(sVol, sDateSt, sDateEn, sMetric string) []string {
+func aws_get_metric_statistics(sName, sDateSt, sDateEn, sMetric string, sNamespace string) []string {
 	var sCmdAws = "aws"
-	var sCmd = "cloudwatch get-metric-statistics --period 3600 --namespace AWS/EBS --statistics Maximum --dimensions Name=VolumeId,Value=%s --start-time %s --end-time %s --output text --metric-name %s"
-	sCmd1 := fmt.Sprintf(sCmd, sVol, sDateSt, sDateEn, sMetric)
+	var sCmd = "cloudwatch get-metric-statistics --period 3600 --statistics Maximum --dimensions Name=%s --start-time %s --end-time %s --output text --metric-name %s --namespace AWS/%s"
+	sCmd1 := fmt.Sprintf(sCmd, sName, sDateSt, sDateEn, sMetric, sNamespace)
 	fmt.Printf("Running command: '%s %s'\n", sCmdAws, sCmd1)
 	cmd := exec.Command(sCmdAws, strings.Split(sCmd1, " ")...)
 	var outErr bytes.Buffer
